008e234976c63f648cb9fbff6e90583c77f6e086
Dan <dan@danitt.com>
2018-07-27 16:12:20 +1000

    bookmarks, users routes

--- app/helpers/bookmark-title.js
+++ app/helpers/bookmark-title.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function bookmarkTitle([title, about]/*, hash*/) {
-  return about === null ? title : `${title} - ${about}`;
+  return about ? `${title} - ${about}` : title;
 }
 
 export default helper(bookmarkTitle);
---
+++ app/models/bookmark.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+// import { belongsTo, hasMany} from 'ember-data/relationships';
+
+export default DS.Model.extend({
+  link: DS.attr('string'),
+  title: DS.attr('string'),
+  about: DS.attr('string'),
+  public: DS.attr('boolean'),
+  created: DS.attr('date', {
+    defaultValue: () => new Date(),
+  }),
+});
